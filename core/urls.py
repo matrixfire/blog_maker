9694fee04ddb34a1884647dbab581437cafb5444
matrixfire <48238435+matrixfire@users.noreply.github.com>
2024-07-23 22:05:54 +0800

    July 23, 2024, 10:05 PM

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     # Add other paths here
+    path("test/", views.index2, name="index2"),
+    path("sections/<int:num>", views.section, name="section")
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from .models import BlogPost
+from django.http import Http404, HttpResponse
+
 
 # Create your views here.
 def index(request):
     post = BlogPost.objects.first()
     return render(request, 'core/index.html', {"post": post})
+
+
+
+# Create your views here.
+def index2(request):
+    return render(request, "core/index2.html")
+
+# The texts are much longer in reality, but have
+# been shortened here to save space
+texts = ["Text 1", "Text 2", "Text 3"]
+
+def section(request, num):
+    if 1 <= num <= 3:
+        return HttpResponse(texts[num - 1])
+    else:
+        raise Http404("No such section")
